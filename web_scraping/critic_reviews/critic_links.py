0a67d5adff05397cc2b15219b74cede3ddc6a5a2
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2016-07-18 16:13:52 -0600

    critic links completed, looking at how to distribute

--- web_scraping/critic_reviews/critic_links.py
+++ web_scraping/critic_reviews/critic_links.py
@@ -6,17 +6,52 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 import string
 import cPickle as pickle
+import re
+import time
 
-
-
-def get_all_titles(list):
+def get_all_titles():
     """ returns all the movie names from boxofficemojo.com in a list"""
     # The following imports the list of box office mojo URLs
-    list_urls = pd.read_csv('../get_movie_information/summary.txt', header = None, dtype="string")
-    list_urls = np.loadtxt('../get_movie_information/summary.txt', dtype="string")
+    list_urls = pd.read_csv('../get_movie_information/summary.txt', header = None, sep='\t')
 
-    i = soup.find("title")
+    # Set index to urls
+    index = list_urls.values.T.tolist()
 
+    # List of movie urls
+    titles_list = []
+    title_year = []
+    movie_year = []
+    dead_links = []
+
+    # Loop through the pages for each letter
+    for i, link in enumerate(index[0][:]):
+        try:
+            page = urllib2.urlopen(link)
+            soup = BeautifulSoup(page, "html5lib")
+            # Removing "- Box Office Mojo" from the title names
+            title = soup.find("title").contents[0].strip('- Box Office Mojo')
+
+                # print i
+                # print title[:-6]
+                # print title[-6:].strip("()")
+
+            # Split title into name and year
+            titles_list.append(title[:-6])
+            title_year.append(title[:-6] + ", " + title[-6:].strip("()"))
+            movie_year.append(title[-6:].strip("()"))
+            time.sleep(0.2)
+
+        except Exception, e:
+            print link
+            dead_links.append(link)
+            logging.exception(e)
+
+    return titles_list, title_year, movie_year
+
+# def critic_review_links():
+#
 
 if __name__ == '__main__':
-    main()
+    titles_list, title_year, movie_year = get_all_titles()
+
+# seperate timeouts on aws, compartmentalize code
