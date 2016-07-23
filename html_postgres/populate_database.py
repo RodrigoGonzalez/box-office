0dd083c5eb4f9bdf3d071ff8a692ce3aa7ebd2df
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2016-07-23 10:49:10 -0600

    populate database updated

--- html_postgres/populate_database.py
+++ html_postgres/populate_database.py
@@ -9,12 +9,27 @@ import re
 import time
 import csv
 from unidecode import unidecode
+import js2py
 
 f = codecs.open("index.html?page=daily&view=chart&id=marvel2016.htm&adjust_yr=2016&p=.htm", 'r',"utf-8")
+def get_revenue_table(file):
+    soup = BeautifulSoup(f, "html5lib")
+    java_text = soup.find_all(type="text/javascript")
+    t = java_text[5]
+    jtext = t.getText().split('\t')[6].replace("\n", " ")
+    js2py.eval_js(jtext)
+    revenue = table.to_list()
+    title = soup.title.get_text().replace(" - Daily Box Office Results - Box Office Mojo", "")
+    r = soup.body.find(id="container").find(id="main").find(id="body").select("table")[2].tbody.tr.td.select("table")[0].tbody.tr.center.tbody.b
+    total_revenues = re.sub('[!@#$,]', '', r.get_text())
 
-soup = BeautifulSoup(f, "html5lib")
-tag = soup.script
+    return unidecode(title), int(total_revenues)
 
-print tag.keys()
 
-# soup.body.find(id="container").find(id="main").find(id="body").contents[-2].tbody.tr.td
+    from urllib2 import Request, urlopen, URLError
+
+
+    # To strip html from critic reviews
+
+    url = 'http://christylemire.com/captain-america-civil-war/'
+    page = urllib2.urlopen(url)
