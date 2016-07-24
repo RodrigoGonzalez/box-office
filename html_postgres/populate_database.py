ace6e1050f7e9dd8c541010bf0295cc2f9bd531e
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2016-07-23 20:34:17 -0600

    finished cleaning html data

--- html_postgres/populate_database.py
+++ html_postgres/populate_database.py
@@ -14,8 +14,10 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 from urllib2 import Request, urlopen, URLError
 from datetime import datetime
+import psycopg2
+import os
+import boto
 
-missing_information = []
 
 def get_revenue_table(file):
     f = codecs.open(file, 'r',"utf-8")
@@ -34,7 +36,13 @@ def get_revenue_table(file):
         runtime_pre = production_budget = re.sub('[!@#$,.a-zA-z]', '', info[4].get_text()).strip().split()
         runtime = int(runtime_pre[0]) * 60 + int(runtime_pre[1])
         MPAA = production_budget = re.sub('[!@#$,]', '', info[5].get_text())
-        production_budget = re.sub('[!@#$,]', '', info[6].get_text())
+
+        # Convert production budget string to integer
+        production_budget_pre1 = unidecode(re.sub('[!@#$,]', '', info[6].get_text()))
+        production_budget_pre2 = "".join(production_budget_pre1.lower().split())
+        for word, initial in {"million":"000000", "thousand":"000" }.items():
+            production_budget = production_budget_pre2.replace(word.lower(), initial)
+        print production_budget
 
         # Extract revenue figures
         java_text = soup.find_all(type="text/javascript")
@@ -42,29 +50,55 @@ def get_revenue_table(file):
         jtext = t.getText().split('\t')[6].replace("\n", " ")
         table = js2py.eval_js(jtext)
         revenue = table.to_list()
+
         # Enter into dataframe
         rev = pd.DataFrame(revenue)
         rev.drop(0, axis=1, inplace=True)
+
         # Get total base revenues
         base_revenues = rev[1].sum()
+
         # Calculate conversion factor
         rev_cf = (total_revenues / base_revenues) / 1000000
 
-        labels = ['title1', 'title2', 'total_revenues', 'distributor', 'dt_obj', 'genre', 'runtime', 'MPAA', 'production_budget', 'rev', 'rev_cf']
+        # Load onto dictionary to export
+        keys = ['title1', 'title2', 'total_revenues', 'distributor', 'dt_obj', 'genre', 'runtime', 'MPAA', 'production_budget', 'revenues', 'rev_cf']
+
+        values = [title1, title2, total_revenues, distributor, dt_obj, genre, runtime, MPAA, production_budget, rev * rev_cf, rev_cf]
+
+        movie_details = dict(zip(keys, values))
+
     except Exception, e:
         print file
+        movie_details = {'title1': title1}
         missing_information.append(file)
         logging.exception(e)
 
-    return title2, rev
+    return movie_details
 
+def populate_postgres(link, df):
+    d = get_revenue_table(link)
+    if len(d) > 1:
+        pass
+        df.append(df2, ignore_index=True)
+    else:
+        missing_information.append(d['title1'])
 
 
 if __name__ == '__main__':
-    link = "index.html?page=daily&view=chart&id=marvel2016.htm&adjust_yr=2016&p=.htm"
+    # dead link example
+    # link = "index.html?page=daily&view=chart&id=1dwhereweare.htm&adjust_yr=2016&p=.htm"
+    missing_information = []
 
-    title, tot_rev = get_revenue_table(link)
+    link = "index.html?page=daily&view=chart&id=gladiator.htm&adjust_yr=2016&p=.htm"
+    d = get_revenue_table(link)
 
+    # Connect to S3
+    # access_key, access_secret_key = os.getenv('AWS_ACCESS_KEY_ID'), os.getenv('AWS_SECRET_ACCESS_KEY')
+    # conn = boto.connect_s3(access_key, access_secret_key)
+    # # List all the buckets
+    # all_buckets = [b.name for b in conn.get_all_buckets()]
+    # print all_buckets
 
     #
     # # To strip html from critic reviews
