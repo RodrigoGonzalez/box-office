b3dd5c093e88d10a47c00290b48c7ea113de843b
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2016-07-28 19:35:53 -0600

    finished loading criti reviews

--- html_postgres/critic_reviews.py
+++ html_postgres/critic_reviews.py
@@ -3,10 +3,12 @@ import numpy as np
 from bs4 import BeautifulSoup
 import codecs
 from unidecode import unidecode
-import re
+import re, os
 import requests
 import bleach
 from __future__ import division
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def load_dataframe():
     df = pd.read_csv('movie_revs.csv')
@@ -23,40 +25,48 @@ def include_review(title, main_text):
     return x
 
 def extract_text(r):
+    try:
+        r = requests.get(link)
+        stat_code = r.status_code
+        f = r.text
 
-    f = r.text
+        soup = BeautifulSoup(f, "html5lib")
 
-    soup = BeautifulSoup(f, "html5lib")
+        # kill all script and style elements
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
 
-    # kill all script and style elements
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
+        # get text
+        txt = soup.get_text()
 
-    # get text
-    txt = soup.get_text()
+        # bleach out the remaining links
+        bleached = bleach.clean(txt, strip=True)
 
-    # bleach out the remaining links
-    bleached = bleach.clean(txt, strip=True)
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in bleached.splitlines())
 
-    # break into lines and remove leading and trailing space on each
-    lines = (line.strip() for line in bleached.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 
-    # break multi-headlines into a line each
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        text = '\n'.join(chunk for chunk in chunks if chunk)
 
-    # drop blank lines
-    text = '\n'.join(chunk for chunk in chunks if chunk)
+        # clean
+        white = re.sub('[!@#$:,&*]', '', text)
 
-    # clean
-    white = re.sub('[!@#$:,&*]', '', text)
+        # clean some more
+        snow_white = unidecode(re.sub('\s', ' ', white))
 
-    # clean some more
-    snow_white = unidecode(re.sub('\s', ' ', white))
+        # perfect
+        perfect = re.sub('[*\']', '', snow_white)
 
-    # perfect
-    perfect = re.sub('[*\']', '', snow_white)
+        return perfect, status_code
 
-    return perfect
+    except Exception, e:
+        missing_information.append(link)
+        logging.exception(e)
+        perfect = '0'
+        return perfect, status_code
 
 def populate(link):
     r = requests.get(link)
@@ -66,13 +76,44 @@ def populate(link):
     else:
         return None
 
-def list_of_links():
-    
-    pass
+def list_of_links(file):
+    data = []
+    stat_codes = []
+    with open(file) as f:
+        links = [re.sub('[\n]', "", link) for link in f if link != '0']
+    links.pop(0)
+
+    for link in links:
+        perfect, status_code = extract_text(link)
+        data.append(perfect)
+        stat_codes.append(status_code)
+    review_data = zip(data, links, stat_codes)
+    return review_data
 
 if __name__ == '__main__':
+    missing_information = []
+    done = []
     df = load_dataframe()
-    columns = ['id', 'movie_key', 'critic', 'review']
+    columns = ['id', 'movie_key', 'critic', 'link', 'review', 'status_code']
     df_reviews = pd.DataFrame(columns=columns)
 
-    for
+    path = '../critic_links2'
+
+    for filename in os.listdir(path):
+        file = path + "/" + filename
+        columns = ['review', 'link', 'status_code']
+        review_data = pd.DataFrame(list_of_links(file), columns, index=[1,])
+        id_base = re.sub('.txt', '', filename)
+        review_data['id'] = [id_base + str(item) for item in xrange(review_data.shape[0])]
+        review_data['movie_key'] = re.sub('.txt', '', filename)
+        review_data['critic'] = [link.split('.')[1]  for link in review_data['link']]
+        df_reviews = pd.concat([df_reviews, review_data], axis=0)
+        df_reviews = df_reviews.append(review_data, ignore_index=True)
+        done.append(filename)
+
+    df_reviews.to_csv('critic_reviews.csv')
+    movies_done = pd.DataFrame(done, index=[1,])
+    movies_done.to_csv('movies_done.csv')
+
+    missing_info = pd.DataFrame(missing_information, index=[1,])
+    missing_info.to_csv('missing_info.csv')
