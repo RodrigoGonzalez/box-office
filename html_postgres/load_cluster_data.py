748c89d1b50f21056547330c8d7e5ba465fc1d4e
RodrigoGonzalez <rodrigo.gonzalez@colorado.edu>
2016-07-29 17:30:27 -0600

    compressed files for transfer

--- html_postgres/load_cluster_data.py
+++ html_postgres/load_cluster_data.py
@@ -22,27 +22,57 @@ def connect():
 def execute_find_movie(title, year):
 	# execute our Query
 	try:
-		cursor.execute("SELECT * FROM title WHERE title like title AND production_year = year AND kind_id = 1 LIMIT 15")
-		records = cursor.fetchall()
+		cursor.execute("SELECT * FROM title WHERE title = '{}' AND production_year = '{}' AND kind_id = 1 LIMIT 15".format(title, year))
+		records1 = cursor.fetchall()
 	except:
     	conn.rollback()
-		print "error: " + title
-		records = None
+		print "error1: " + title
+	try:
+		cursor.execute("SELECT * FROM aka_title WHERE title = '{}' AND production_year = '{}' AND kind_id = 1 LIMIT 15".format(title, year))
+		records2 = cursor.fetchall()
+	except:
+		conn.rollback()
+		print "error 2: " + title
+	records = records1 + records2
+
+	if len(records) == 0:
+		titles_to_redo.append([title, year])
 	return records
 
 def prepare_title(title):
-
+	new_title = None
 	return new_title
-
-def first_pass():
-	pass
 
 if __name__ == "__main__":
 	conn, cursor = connect()
 	df = pd.read_csv('search_titles.csv')
+	df.drop('Unnamed: 0', axis=1, inplace=True)
+	titles_to_redo = []
 
+	columns = ['id', 'title', 'production_year', 'md5sum']
+	df_records = pd.DataFrame(columns = columns)
 
+	psql_cols = ['id', 'title', 'imdb_index', 'kind_id', 'production_year', 'imdb_id', 'phonetic_code', 'episode_of_id', 'season_nr', 'episode_nr', 'series_years', 'md5sum']
 
-where movetitle like '%10,000%'
+	for title, year in df.iteritems():
+		records = execute_find_movie(title, year)
+		if len(records) == 0:
+			pass
+		else:
+			df_titles = pd.DataFrame(records, columns=psql_cols)
+			df2 = df_titles[columns]
+			df_records = df_records.append(df2, ignore_index=True)
 
-cursor.execute("SELECT * FROM title WHERE title = 'The Powerpuff Girls Movie' AND production_year = 2013 AND kind_id = 1 LIMIT 15")
+	df_records[['id','production_year']] = ..astype(int)
+	df_records.to_csv('cluster_data.csv', mode = 'w', index=False)
+
+# where movetitle like '%10,000%'
+#
+# cursor.execute("SELECT * FROM aka_title WHERE title = 'Legally Blonde 2: Red, White and Blonde' AND production_year = 2014 AND kind_id = 1 LIMIT 15")
+
+# cursor.execute("SELECT * FROM title WHERE title = 'After Earth' LIMIT 15")
+# records1 = cursor.fetchall()
+# cursor.execute("SELECT * FROM title WHERE title like 'Hunger Games%' LIMIT 15")
+# records2 = cursor.fetchall()
+
+   ['id', 'title', 'imdb_index', 'kind_id', 'production_year', 'imdb_id', 'phonetic_code', 'episode_of_id', 'season_nr', 'episode_nr', 'series_years', 'md5sum']
